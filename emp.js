7f15274f02e2606a73d1e598ddd00c9cd5a8be2b
Matthew Palka <50680920+mpalka1@users.noreply.github.com>
2019-12-03 19:37:14 -0600

    Update emp.js

--- emp.js
+++ emp.js
@@ -18,3 +18,42 @@ class Employee {
     }
 }
 module.exports = Employee;
+
+function appMenu() {
+  function createManager() {
+    inquirer.prompt([
+      /* prompt choices here */
+    ]).then(answers => {
+      /* build manager */
+      createTeam();
+    });
+  }
+  function createTeam() {
+    inquirer.prompt([
+      /* prompt choice here */
+    ]).then(userChoice => {
+      /* call one function below based on choice */
+    });
+  }
+  function addEngineer() {
+    inquirer.prompt([
+      /* prompts here */.
+    ]).then(answers => {
+      /* create engineer */
+      createTeam();
+    });
+  }
+  function addIntern() {
+    inquirer.prompt([
+      /* prompts here */
+    ]).then(answers => {
+      /* create interin */
+      createTeam();
+    });
+  }
+  function buildTeam() {
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
+  createManager();
+}
+appMenu();
